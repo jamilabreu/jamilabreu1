81e982ac8175e91131682b7fd3530d060c230d4d
jamilabreu <abreu.jamil@gmail.com>
2019-05-20 22:22:15 -0400

    now

--- pages/index.js
+++ pages/index.js
@@ -108,7 +108,7 @@ function Home() {
       </div>
       <style global jsx>{`
         .social a {
-          color: #ddd;
+          color: #ccc;
         }
         .social a:first-child:hover {
           color: #2ebe60;
