c005d5a85cbfdb5ca91895c9464f79db45416888
jamilabreu <abreu.jamil@gmail.com>
2019-05-20 22:40:36 -0400

    icons

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
             integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
-            crossorigin="anonymous"
+            crossOrigin="anonymous"
           />
         </Head>
         <body
